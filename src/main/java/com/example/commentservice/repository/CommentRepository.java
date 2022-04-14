b42eecc70268c7386ee2a1398c7d28ef577e46f6
kalpanag <mendarajeshk@maveric-systems.com>
2022-04-14 09:50:18 +0530

    getbyId added

--- src/main/java/com/example/commentservice/repository/CommentRepository.java
+++ src/main/java/com/example/commentservice/repository/CommentRepository.java
@@ -1,15 +1,14 @@
 package com.example.commentservice.repository;
 
 import com.example.commentservice.entity.Comment;
-import org.springframework.data.domain.Pageable;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.Collection;
-import java.util.List;
+
 
 public interface CommentRepository extends MongoRepository<Comment, String> {
 
-    public List<Comment> findBypostID(String postID, Pageable page);
+
 
 
 
